8bb7eedcc10b455684d299eb9ed2523f193dab91
Bojan <bdimosky35@yahoo.com>
2024-07-14 19:10:59 +0200

    Update dependencies and add Clerk webhook route

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["sign-in", "sign-up"]);
+const isPublicRoute = createRouteMatcher([
+  "sign-in",
+  "sign-up",
+  "/api/webhooks/clerk",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) auth().protect();
