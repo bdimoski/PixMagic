ea26b5f19ece64139ba12a9302c33eaa61e1f933
Bojan <bdimosky35@yahoo.com>
2024-07-14 12:54:01 +0200

    add MobileNav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,9 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <p>home</p>
-      <UserButton afterSwitchSessionUrl='/' />
     </div>
   )
 }
