3e76d63b9ba5619046c8187926f6eeb9abd714f0
Bojan <bdimosky35@yahoo.com>
2024-07-14 11:09:09 +0200

    chore: Add Clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,10 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
