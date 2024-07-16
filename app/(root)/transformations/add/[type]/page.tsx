34d9b75192b06710c798e4c754c313c5273df639
Bojan <bdimosky35@yahoo.com>
2024-07-16 10:06:52 +0200

    Update npm dependencies and add form components for custom fields

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,14 +2,33 @@ import React from 'react'
 
 import Header from '@/components/shared/Header'
 import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { auth } from '@clerk/nextjs/server'
+import { getUserById } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+    const { userId } = auth()
+    if (!userId) redirect('/sign-in')
     const transformation = transformationTypes[type]
+
+    const user = await getUserById(userId);
+
     return (
-        <Header
-            title={transformation.title}
-            subtitle={transformation.subTitle}
-        />
+        <>
+            <Header
+                title={transformation.title}
+                subtitle={transformation.subTitle}
+            />
+            <section className='mt-10'>
+                <TransformationForm
+                    action="Add"
+                    userId={user._id}
+                    type={transformation.type as TransformationTypeKey}
+                    creditBalance={user.creditBalance}
+                />
+            </section>
+        </>
     )
 }
 
