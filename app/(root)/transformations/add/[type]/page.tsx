76054ac4181e3eb78b48c03059c9bb6d7f4536a2
Bojan <bdimosky35@yahoo.com>
2024-07-16 08:54:59 +0200

    Add Header component for transformation type page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const AddTransformationTypePage = () => {
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
+
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+    const transformation = transformationTypes[type]
     return (
-        <div>AddTransformationTypePage</div>
+        <Header
+            title={transformation.title}
+            subtitle={transformation.subTitle}
+        />
     )
 }
 
